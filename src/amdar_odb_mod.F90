b5fb7b3114adc051b3e6ede81462b40ea3e8c8fe
Li Dong <dongli@lasg.iap.ac.cn>
2018-10-24 15:26:59 +0800

    Fix ODB datetime
    ODB’s string only accept 8 characters, so I split datetime into two parts.

--- src/amdar_odb_mod.F90
+++ src/amdar_odb_mod.F90
@@ -23,7 +23,7 @@ contains
     character(100) odb_unparsed_sql
     character(50) odb_file_name
 
-    character(30) time_str
+    character(30) str
     type(linked_list_iterator_type) record_iterator
 
     odb_file_name = 'amdar.odb'
@@ -31,35 +31,36 @@ contains
     ! Write ODB file.
     call odbql_open('', odb_db)
     call odbql_prepare_v2(odb_db, 'CREATE TABLE amdar AS (' // &
-      'flight_name STRING, lon REAL, lat REAL, z REAL, time STRING, ' // &
+      'flight_name STRING, lon REAL, lat REAL, z REAL, date STRING, time STRING, ' // &
       'amdar_temperature REAL, ' // &
       'amdar_wind_speed REAL, ' // &
       'amdar_wind_direction REAL, ' // &
       'amdar_specific_humidity REAL) ON "' // &
       trim(odb_file_name) // '";', -1, odb_stmt, odb_unparsed_sql)
     call odbql_prepare_v2(odb_db, 'INSERT INTO amdar (' // &
-      'flight_name, lon, lat, z, time, ' // &
+      'flight_name, lon, lat, z, date, time, ' // &
       'amdar_temperature, ' // &
       'amdar_wind_speed, ' // &
       'amdar_wind_direction, ' // &
       'amdar_specific_humidity' // &
-      ') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);', -1, odb_stmt, odb_unparsed_sql)
+      ') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', -1, odb_stmt, odb_unparsed_sql)
 
     record_iterator = linked_list_iterator(records)
     do while (.not. record_iterator%ended())
       select type (record => record_iterator%value)
       type is (amdar_record_type)
-        call odbql_bind_text  (odb_stmt, 1, record%flight%name, len_trim(record%flight%name))
-        call odbql_bind_double(odb_stmt, 2, dble(record%lon))
-        call odbql_bind_double(odb_stmt, 3, dble(record%lat))
-        call odbql_bind_double(odb_stmt, 4, dble(record%z))
-        time_str = record%time%format('%Y%m%d%H%M')
-        time_str = 'N/A'
-        call odbql_bind_text  (odb_stmt, 5, time_str, len_trim(time_str))
-        call odbql_bind_double(odb_stmt, 6, dble(record%amdar_temperature))
-        call odbql_bind_double(odb_stmt, 7, dble(record%amdar_wind_speed))
-        call odbql_bind_double(odb_stmt, 8, dble(record%amdar_wind_direction))
-        call odbql_bind_double(odb_stmt, 9, dble(record%amdar_specific_humidity))
+        call odbql_bind_text  (odb_stmt,  1, record%flight%name, len_trim(record%flight%name))
+        call odbql_bind_double(odb_stmt,  2, dble(record%lon))
+        call odbql_bind_double(odb_stmt,  3, dble(record%lat))
+        call odbql_bind_double(odb_stmt,  4, dble(record%z))
+        str = record%time%format('%Y%m%d')
+        call odbql_bind_text  (odb_stmt,  5, trim(str), len_trim(str))
+        str = record%time%format('%Y%m%d')
+        call odbql_bind_text  (odb_stmt,  6, trim(str), len_trim(str))
+        call odbql_bind_double(odb_stmt,  7, dble(record%amdar_temperature))
+        call odbql_bind_double(odb_stmt,  8, dble(record%amdar_wind_speed))
+        call odbql_bind_double(odb_stmt,  9, dble(record%amdar_wind_direction))
+        call odbql_bind_double(odb_stmt, 10, dble(record%amdar_specific_humidity))
         call odbql_step(odb_stmt)
       class default
         write(*, *) '[Error]: Unknown record in the list!'
--- src/synop_odb_mod.F90
+++ src/synop_odb_mod.F90
@@ -17,10 +17,10 @@ contains
     type(odbql) odb_db
     type(odbql_stmt) odb_stmt
     type(odbql_value) odb_value
-    character(100) odb_unparsed_sql
+    character(100) :: odb_unparsed_sql = ''
     character(50) odb_file_name
 
-    character(30) time_str
+    character(30) str
     type(linked_list_iterator_type) record_iterator
 
     odb_file_name = 'synop.odb'
@@ -28,7 +28,7 @@ contains
     ! Write ODB file.
     call odbql_open('', odb_db)
     call odbql_prepare_v2(odb_db, 'CREATE TABLE synop AS (' // &
-      'station_name STRING, lon REAL, lat REAL, z REAL, time STRING, ' // &
+      'station_name STRING, lon REAL, lat REAL, z REAL, date STRING, time STRING, ' // &
       'sfc_temperature REAL, ' // &
       'sfc_dewpoint REAL, ' // &
       'sfc_wind_speed REAL, ' // &
@@ -36,13 +36,13 @@ contains
       'sfc_pressure REAL, ' // &
       ') ON "' // trim(odb_file_name) // '";', -1, odb_stmt, odb_unparsed_sql)
     call odbql_prepare_v2(odb_db, 'INSERT INTO synop (' // &
-      'station_name, lon, lat, z, time, ' // &
+      'station_name, lon, lat, z, date, time, ' // &
       'sfc_temperature, ' // &
       'sfc_dewpoint, ' // &
       'sfc_wind_speed, ' // &
       'sfc_wind_direction, ' // &
       'sfc_pressure' // &
-      ') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', -1, odb_stmt, odb_unparsed_sql)
+      ') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', -1, odb_stmt, odb_unparsed_sql)
 
     record_iterator = linked_list_iterator(records)
     do while (.not. record_iterator%ended())
@@ -52,14 +52,15 @@ contains
         call odbql_bind_double(odb_stmt,  2, dble(record%station%lon))
         call odbql_bind_double(odb_stmt,  3, dble(record%station%lat))
         call odbql_bind_double(odb_stmt,  4, dble(record%station%z))
-        time_str = record%time%format('%Y%m%d%H%M')
-        time_str = 'N/A'
-        call odbql_bind_text  (odb_stmt,  5, time_str, len_trim(time_str))
-        call odbql_bind_double(odb_stmt,  6, dble(record%sfc_temperature))
-        call odbql_bind_double(odb_stmt,  7, dble(record%sfc_dewpoint))
-        call odbql_bind_double(odb_stmt,  8, dble(record%sfc_wind_speed))
-        call odbql_bind_double(odb_stmt,  9, dble(record%sfc_wind_direction))
-        call odbql_bind_double(odb_stmt, 10, dble(record%sfc_pressure))
+        str = record%time%format('%Y%m%d')
+        call odbql_bind_text  (odb_stmt,  5, trim(str), len_trim(str))
+        str = record%time%format('%H%M%S')
+        call odbql_bind_text  (odb_stmt,  6, trim(str), len_trim(str))
+        call odbql_bind_double(odb_stmt,  7, dble(record%sfc_temperature))
+        call odbql_bind_double(odb_stmt,  8, dble(record%sfc_dewpoint))
+        call odbql_bind_double(odb_stmt,  9, dble(record%sfc_wind_speed))
+        call odbql_bind_double(odb_stmt, 10, dble(record%sfc_wind_direction))
+        call odbql_bind_double(odb_stmt, 11, dble(record%sfc_pressure))
         call odbql_step(odb_stmt)
       class default
         write(*, *) '[Error]: Unknown record in the list!'
