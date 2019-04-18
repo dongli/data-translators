78dc1e797b74bc9d601d569e0c2851f547296d9c
Li Dong <dongli@lasg.iap.ac.cn>
2019-04-18 19:33:30 +0800

    Output RH in synop odb

--- src/synop_odb_mod.F90
+++ src/synop_odb_mod.F90
@@ -31,39 +31,43 @@ contains
     ! Write ODB file.
     call odbql_open('', odb_db)
     call odbql_prepare_v2(odb_db, 'CREATE TABLE synop AS (' // &
-      'platform_id STRING, ' // &               !  1
-      'lon REAL, ' // &                         !  2
-      'lat REAL, ' // &                         !  3
-      'z REAL, ' // &                           !  4
-      'date YYYYMMDD, ' // &                    !  5
-      'time HHMMSS, ' // &                      !  6
-      'type INTEGER, ' // &                     !  7
-      'temperature REAL, ' // &                 !  8
-      'temperature_qc INTEGER, ' // &           !  9
-      'dewpoint REAL, ' // &                    ! 10
-      'wind_u REAL, ' // &                      ! 11
-      'wind_v REAL, ' // &                      ! 12
-      'wind_qc INTEGER, ' // &                  ! 13
-      'pressure REAL, ' // &                    ! 14
-      'pressure_qc INTEGER, ' // &              ! 15
+      'platform_id STRING, '               // & !  1
+      'lon REAL, '                         // & !  2
+      'lat REAL, '                         // & !  3
+      'z REAL, '                           // & !  4
+      'date YYYYMMDD, '                    // & !  5
+      'time HHMMSS, '                      // & !  6
+      'type INTEGER, '                     // & !  7
+      'temperature REAL, '                 // & !  8
+      'temperature_qc INTEGER, '           // & !  9
+      'dewpoint REAL, '                    // & ! 10
+      'relative_humidity REAL, '           // & ! 11
+      'relative_humidity_qc INTEGER, '     // & ! 12
+      'wind_u REAL, '                      // & ! 13
+      'wind_v REAL, '                      // & ! 14
+      'wind_qc INTEGER, '                  // & ! 15
+      'pressure REAL, '                    // & ! 16
+      'pressure_qc INTEGER '               // & ! 17
       ') ON "' // trim(file_path) // '";', -1, odb_stmt, odb_unparsed_sql)
     call odbql_prepare_v2(odb_db, 'INSERT INTO synop (' // &
-      'platform_id, ' // &                      !  1
-      'lon, ' // &                              !  2
-      'lat, ' // &                              !  3
-      'z, ' // &                                !  4
-      'date, ' // &                             !  5
-      'time, ' // &                             !  6
-      'type, ' // &                             !  7
-      'temperature, ' // &                      !  8
-      'temperature_qc, ' // &                   !  9
-      'dewpoint, ' // &                         ! 10
-      'wind_u, ' // &                           ! 11
-      'wind_v, ' // &                           ! 12
-      'wind_qc, ' // &                          ! 13
-      'pressure' // &                           ! 14
-      'pressure_qc' // &                        ! 15
-      ') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', -1, odb_stmt, odb_unparsed_sql)
+      'platform_id, '                      // & !  1
+      'lon, '                              // & !  2
+      'lat, '                              // & !  3
+      'z, '                                // & !  4
+      'date, '                             // & !  5
+      'time, '                             // & !  6
+      'type, '                             // & !  7
+      'temperature, '                      // & !  8
+      'temperature_qc, '                   // & !  9
+      'dewpoint, '                         // & ! 10
+      'relative_humidity, '                // & ! 11
+      'relative_humidity_qc, '             // & ! 12
+      'wind_u, '                           // & ! 13
+      'wind_v, '                           // & ! 14
+      'wind_qc, '                          // & ! 15
+      'pressure,'                          // & ! 16
+      'pressure_qc'                        // & ! 17
+      ') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', -1, odb_stmt, odb_unparsed_sql)
 
     record_iterator = linked_list_iterator(records)
     do while (.not. record_iterator%ended())
@@ -81,11 +85,13 @@ contains
         call odbql_bind_double(odb_stmt,  8, dble(record%sfc_temperature))
         call odbql_bind_int   (odb_stmt,  9, record%sfc_temperature_qc)
         call odbql_bind_double(odb_stmt, 10, dble(record%sfc_dewpoint))
-        call odbql_bind_double(odb_stmt, 11, dble(record%sfc_wind_u))
-        call odbql_bind_double(odb_stmt, 12, dble(record%sfc_wind_v))
-        call odbql_bind_int   (odb_stmt, 13, record%sfc_wind_qc)
-        call odbql_bind_double(odb_stmt, 14, dble(record%sfc_pressure))
-        call odbql_bind_int   (odb_stmt, 15, record%sfc_pressure_qc)
+        call odbql_bind_double(odb_stmt, 11, dble(record%sfc_relative_humidity))
+        call odbql_bind_int   (odb_stmt, 12, record%sfc_relative_humidity_qc)
+        call odbql_bind_double(odb_stmt, 13, dble(record%sfc_wind_u))
+        call odbql_bind_double(odb_stmt, 14, dble(record%sfc_wind_v))
+        call odbql_bind_int   (odb_stmt, 15, record%sfc_wind_qc)
+        call odbql_bind_double(odb_stmt, 16, dble(record%sfc_pressure))
+        call odbql_bind_int   (odb_stmt, 17, record%sfc_pressure_qc)
         call odbql_step(odb_stmt)
       class default
         write(*, *) '[Error]: Unknown record in the list!'
